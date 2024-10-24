cf94fa6260a040670a43076bea1f5bb960b68915
ColbyD <colbydomingos@my.whccd.edu>
2024-10-23 18:49:16 -0700

    Finished js

---
+++ js/ESV.js
@@ -0,0 +1,13 @@
+myGreeting = "Hola Como Estan";
+
+console.log(myGreeting);
+
+const pi = 3.14;
+
+//pi = 22222; CANNOT CHANGE A CONST AS IT CAN AND WILL ALWAYS ONLY BE WHAT IT IS DEFINED AS
+
+myNumber = 23;
+
+port = Math.min(pi, myNumber) // IT CHOOSES THE SMALLER NUMBER THAT IS WHY THE OUTPUT IS PI
+
+console.log(port)
